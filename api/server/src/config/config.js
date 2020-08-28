017baae277631fd7c890061a93459c20374bbe53
larissamiyaji <natsumi-23@hotmail.com>
2020-08-28 17:54:20 -0300

    Deploy

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -1,26 +1,31 @@
+require("dotenv").config();
+
 module.exports = {
-  "development": {
-    "username": "postgres",
-    "password": null,
-    "database": "burger_queen_development",
-    "host": "db",
-    "dialect": "postgres",
-    "operatorsAliases": false
+  development: {
+    username: "postgres",
+    password: null,
+    database: "burger_queen_development",
+    host: "db",
+    dialect: "postgres",
+    operatorsAliases: false,
   },
-  "test": {
-    "username": "postgres",
-    "password": null,
-    "database": "burger_queen_test",
-    "host": "db",
-    "dialect": "postgres",
-    "operatorsAliases": false
+  test: {
+    username: "postgres",
+    password: null,
+    database: "burger_queen_test",
+    host: "db",
+    dialect: "postgres",
+    operatorsAliases: false,
   },
-  "production": {
-    "username": "postgres",
-    "password": null,
-    "database": "burger_queen_production",
-    "host": "db",
-    "dialect": "postgres",
-    "operatorsAliases": false
-  }
-}
+  production: {
+    username: "process.env.DB_USER",
+    password: "process.env.DB_PASS",
+    database: "process.env.DB_DATABASE",
+    host: "process.env.DB_HOST",
+    dialect: "postgres",
+    operatorsAliases: false,
+    dialectOptions: {
+      ssl: true
+    }
+  },
+};
